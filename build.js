dcac2a7b0d64521eb02beb80a0bf508ba3732d65
Colin Reeder <vpzomtrrfrt@gmail.com>
2017-10-30 13:55:06 -0600

    Remove empty word from end of array

--- build.js
+++ build.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const words = fs.readFileSync('words.txt')
 .toString()
 .split("\n")
-.map(x => x.trim());
+.map(x => x.trim())
+.filter(x => x.length > 0);
 
 const json = JSON.stringify(words);
 fs.writeFileSync("words.json", json);
